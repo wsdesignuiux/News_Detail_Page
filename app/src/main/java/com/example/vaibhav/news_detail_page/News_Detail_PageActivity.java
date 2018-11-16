7c5b7df27029ce9675d0c7e94ea7cfd6518eb8b2
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-16 14:24:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/news_detail_page/News_Detail_PageActivity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.news_detail_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class News_Detail_PageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news__detail__page);
+    }
+}
